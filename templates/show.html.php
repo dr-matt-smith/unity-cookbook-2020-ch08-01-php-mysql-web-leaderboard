0549204ebbd7d152d24ff327bebf7cdf601ecf2b
dr-matt-smith <matt.smith@itb.ie>
2020-03-17 16:24:48 +0000

    added files to project

---
+++ templates/show.html.php
@@ -0,0 +1,6 @@
+<?php include_once __DIR__ . '/_header.php'; ?>
+<p>
+    id = <?= $player->getId(); ?>
+    <br>username = <?= $player->getUsername(); ?>
+    <br>score = <?= $player->getScore(); ?>
+<hr><a href="index.php">home</a>
